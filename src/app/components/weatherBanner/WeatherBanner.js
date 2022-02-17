9df12074161db892d350185fe9bd688944e11d59
Symon <xsymon.rs@gmail.com>
2022-02-17 21:31:18 +0100

    Added weather banner

--- src/app/components/weatherBanner/WeatherBanner.js
+++ src/app/components/weatherBanner/WeatherBanner.js
@@ -1,17 +1,18 @@
 import {Card} from "react-bootstrap"
 import sunnyWeatherImg from "../../../assets/img/weatherStateImages/sunny weather.jpg"
+import "./weatherBanner.scss";
 
 export const WeatherBanner = ({cityName, date, weatherStatus}) => {
 
     return (
-        <Card>
-            <Card.Img src={imageWeatherMap[weatherStatus]} alt="Card image" />
+        <Card className="weatherBannerCard">
+            <Card.Img  src={imageWeatherMap[weatherStatus]} alt="Card image" />
             <Card.ImgOverlay>
                 <Card.Title>
                     <span>{cityName}</span>
                 </Card.Title>
                 <Card.Text><span>{date}</span></Card.Text>
-                <Card.Text><span>{weatherStatus}</span></Card.Text>
+                <Card.Text className="weatherStatus"><span>{weatherStatus}</span></Card.Text>
             </Card.ImgOverlay>
         </Card>
     )

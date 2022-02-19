43753ba308471bf163fdf1aa0f190d7467953801
Symon <xsymon.rs@gmail.com>
2022-02-19 18:28:13 +0100

    Edited main layout

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { WeatherBanner } from "./app/components/weatherBanner/WeatherBanner";
 import { Container, Row, Col } from "react-bootstrap";
 import "./style/global.scss";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PillarCard from './app/components/pillarCard/PillarCard';
+import IconButton from './app/components/IconButton/IconButton';
 
 function App() {
   return (
     <Container fluid="md">
       <Row>
-        <Col xs={6}>
+        <Col xs={6} style={{position: 'relative'}}>
           <WeatherBanner cityName="London" date="Friday 18, September" weatherStatus="Sunny" />
+          <PillarCard  degree="19°" sideCard top='2rem' left="-4rem" weatherStatus="Sunny" />
         </Col>
         <Col xs={4}>
-
+          <IconButton style={{width: '100%'}} text='Aggiungi città' icon='SquarePlus'/>
         </Col>
       </Row>
     </Container>

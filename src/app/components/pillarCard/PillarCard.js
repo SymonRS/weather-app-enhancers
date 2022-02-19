81ce6704a0f98d074a915d696e2f17b43e4a8a3c
Symon <xsymon.rs@gmail.com>
2022-02-19 18:27:13 +0100

    Added pillar card component

--- src/app/components/pillarCard/PillarCard.js
+++ src/app/components/pillarCard/PillarCard.js
@@ -1,11 +1,20 @@
 import { Card } from "react-bootstrap"
+import imgIconByState from "../../../helpers/imageIconsUtils"
+import "./pillarCard.scss";
 
-const PillarCard = ({weekDay, degree, weatherStatus, sideCard = false }) => {
+const PillarCard = ({weekDay, degree, weatherStatus, sideCard = false, top = '0', left = '0' }) => {
+
+    const weatherImage = imgIconByState(weatherStatus);
+
     return(
-        <Card className={`pillarCard ${sideCard ? 'sided' : ''}`}>
+        <Card className={`pillarCard ${sideCard ? 'sided' : ''}`} style={sideCard ? {top: top, left: left} : {}}>
             <Card.Body>
-
+                {weekDay ? <Card.Text className="weekDay">{weekDay}</Card.Text> : ''}
+                <Card.Text className="degrees">{degree}</Card.Text>
+                <img src={weatherImage} />
             </Card.Body>
         </Card>
     )
 }
+
+export default PillarCard;

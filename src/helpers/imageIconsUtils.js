f93265fd8fc96745a88aa0cde999e24e80421832
Symon <xsymon.rs@gmail.com>
2022-02-20 18:58:20 +0100

    Added real weather states

--- src/helpers/imageIconsUtils.js
+++ src/helpers/imageIconsUtils.js
@@ -1,15 +1,28 @@
 import Sunny from "../assets/img/weatherStateIconsImages/Sunny.png";
 import ModRain from "../assets/img/weatherStateIconsImages/ModRain.png";
 import Cloudy from "../assets/img/weatherStateIconsImages/Cloudy.png";
+import Thunderstorm from "../assets/img/weatherStateIconsImages/Thunderstorm.png"
 
 const imgIconByState = weahterState => {
     return imgIconsWeatherMap[weahterState];
 }
 
 const imgIconsWeatherMap = {
-    'Sunny': Sunny,
-    'ModRain': ModRain,
-    'Cloudy': Cloudy
+    'Thunderstorm':Thunderstorm,
+    'Drizzle': Cloudy,
+    'Rain': ModRain,
+    'Snow': Cloudy,
+    'Clouds': Cloudy,
+    'Mist': Cloudy,
+    'Smoke': Cloudy,
+    'Haze': Cloudy, 
+    'Dust': Cloudy,
+    'Fog': Cloudy,
+    'Sand': Cloudy,
+    'Ash': Cloudy,
+    'Squall': Cloudy,
+    'Tornado': Cloudy,
+    'Clear': Sunny
 }
 
 export default imgIconByState;

4b72e9cb26d6bcc6c97b0b2eab833030157d4b9c
Symon <xsymon.rs@gmail.com>
2022-02-20 13:27:08 +0100

    Added support for newly inserted images

--- src/helpers/imageIconsUtils.js
+++ src/helpers/imageIconsUtils.js
@@ -1,11 +1,15 @@
 import Sunny from "../assets/img/weatherStateIconsImages/Sunny.png";
+import ModRain from "../assets/img/weatherStateIconsImages/ModRain.png";
+import Cloudy from "../assets/img/weatherStateIconsImages/Cloudy.png";
 
 const imgIconByState = weahterState => {
     return imgIconsWeatherMap[weahterState];
 }
 
 const imgIconsWeatherMap = {
-    'Sunny': Sunny
+    'Sunny': Sunny,
+    'ModRain': ModRain,
+    'Cloudy': Cloudy
 }
 
 export default imgIconByState;

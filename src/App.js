b0571d13a0e7b86a062459dd1a32b341f2184939
Symon <xsymon.rs@gmail.com>
2022-02-20 13:28:09 +0100

    Changed layout rules for support very little screen

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ import "./style/global.scss";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PillarCard from './app/components/PillarCard/PillarCard';
 import IconButton from './app/components/IconButton/IconButton';
+import WeatherCard from './app/components/WeatherCard/WaetherCard';
 
 function App() {
   return (
-    <Container fluid="md" style={{padding: '2rem 4rem'}}>
+    <Container fluid="xl"  style={{padding: '2rem'}}>
       <Row>
-        <Col xs={6} style={{position: 'relative'}}>
+        <Col  xs={6} lg={8} style={{position: 'relative'}}>
           <WeatherBanner cityName="London" date="Friday 18, September" weatherStatus="Sunny" />
-          <PillarCard  degree="19°" sideCard top='2rem' left="-4rem" weatherStatus="Sunny" />
+          <PillarCard  degree="19°" sideCard top='6.5rem' left="-2rem" weatherStatus="Sunny" />
         </Col>
-        <Col xs={4}>
-          <IconButton style={{width: '100%'}} text='Aggiungi città' icon='SquarePlus'/>
+        <Col xs={6} lg={4} className="d-flex justify-content-between flex-column" /* style={{padding: "0 3rem"}} */>
+          <div className='d-flex justify-content-center flex-column' style={{height: "8.75rem"}}>
+            <IconButton style={{width: '100%'}} text='Aggiungi città' icon='SquarePlus'/>
+          </div>
+          {/* <IconButton style={{width: '100%'}} text='Aggiungi città' icon='SquarePlus'/> */}
+          <WeatherCard city="Florence" date="Friday 18, September" time="2:38 p.m." degree="18°" weatherStatus="Cloudy" />
+          <WeatherCard city="Venice" date="Friday 18, September" time="2:38 p.m." degree="20°" weatherStatus="ModRain" />
         </Col>
       </Row>
     </Container>

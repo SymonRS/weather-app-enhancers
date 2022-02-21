3718c2951385dfe2ee620de7e3b1b7d51cf80bf8
Symon <xsymon.rs@gmail.com>
2022-02-21 19:18:51 +0100

    Fixed missing file error

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import weatherReducer from "../features/weather/weatherSlice";
 
 export const store = configureStore({
